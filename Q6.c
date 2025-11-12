6c6b607f3d5560e2eb1bb78db5b4da58c067001d
Shaurya24576 <Shaurya.24576@stu.upes.ac.in>
2025-11-12 18:45:48 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int a, b, temp;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("\nAfter swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    return 0;
+}
